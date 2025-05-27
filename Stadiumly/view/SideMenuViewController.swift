00b55e2f232bf508da7272a5cfb87597076b8068
nayeon <skdus725@naver.com>
2025-05-27 11:38:45 +0900

    Feat : Infinite Carousel

---
+++ Stadiumly/view/SideMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SideMenuViewController.swift
+//  Stadiumly
+//
+//  Created by 김나연 on 5/26/25.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
